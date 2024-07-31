cc4cfebca43bdaa1a427465f6c2121a09a92ed69
Meet R. Patel <113010137+Meet-Patel-2000@users.noreply.github.com>
2024-07-31 12:06:47 -0400

    added path for screen1 and screen2 in urls.py

--- ecochefmain/appecochef/urls.py
+++ ecochefmain/appecochef/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('screen1/', views.screen1, name='screen1'),
+    path('screen2/', views.screen2, name='screen2'),
     path('screen4/', views.screen4, name='screen4'),
     path('logout/', views.logout_view, name='logout'),
 
