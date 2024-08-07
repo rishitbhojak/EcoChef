c0cfd92f62c380b928b7143d888dc8528e772d46
Siddh Patel <120444589+SiddhP1033@users.noreply.github.com>
2024-08-06 23:52:38 -0400

    Update urls.py
    Added url for screen3 in urls

--- ecochefmain/appecochef/urls.py
+++ ecochefmain/appecochef/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('screen1/', views.screen1, name='screen1'),
     path('screen2/', views.screen2, name='screen2'),
+    path('screen3/', views.screen3, name='screen3'),
     path('screen4/', views.screen4, name='screen4'),
     path('logout/', views.logout_view, name='logout'),
     path('try-now/', views.try_now_view, name='try_now'),
