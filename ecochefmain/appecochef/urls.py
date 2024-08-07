3a1b244e54c48d5e37e08a0b028382a483905439
Rishit Bhojak <51954932+rishitbhojak@users.noreply.github.com>
2024-08-06 23:48:52 -0400

    Refactored Auth Pages urls

--- ecochefmain/appecochef/urls.py
+++ ecochefmain/appecochef/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('signup/', views.SignupView.as_view(), name='signup'),
+    path('login/', views.LoginView.as_view(), name='login'),
     path('screen1/', views.screen1, name='screen1'),
     path('screen2/', views.screen2, name='screen2'),
     path('screen4/', views.screen4, name='screen4'),
